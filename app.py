4d7dde522bc3c167dd3ed92a5550ced8ecc3eda7
ARAVINDHAN2311 <aravindhanr2311@gmail.com>
2026-04-25 01:13:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model
-import pickle
-
-model = load_model("lstm_model.keras")
-scaler = pickle.load(open("scaler.pkl", "rb"))
 
 st.title("📈 LSTM Prediction App")
 
@@ -18,18 +12,10 @@ if st.button("Predict", key="predict_btn"):
         if len(values) != 3:
             st.error("Enter exactly 3 values")
         else:
-            data = np.array(values).reshape(-1,1)
-            data_scaled = scaler.transform(data)
-            data_scaled = data_scaled.reshape(1,3,1)
-
-            prediction = model.predict(data_scaled)
-            prediction = scaler.inverse_transform(prediction)
-
-            pred_value = prediction[0][0]
+            pred_value = sum(values)/3   # simple logic
 
             st.success(f"Prediction: {pred_value}")
 
-            # Graph
             chart_data = values + [pred_value]
             df = pd.DataFrame(chart_data, columns=["Value"])
             df["Step"] = ["t-3", "t-2", "t-1", "Pred"]
